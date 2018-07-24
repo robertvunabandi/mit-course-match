34ae0996bc687f70890690dd4fc3f6824b6ae6cd
Robert Vunabandi <rvunabandi@gmail.com>
2018-07-23 22:16:58 -0700

    made config private in sql

--- sql/sql.py
+++ sql/sql.py
@@ -3,11 +3,11 @@ import mysql.connector
 # connect to mysql database for MITCourseMatch
 # run: mysql -u root -p, then type password
 # todo - add connection error checks here...
-config = {
+_config = {
 	'host': 'localhost',
 	'user': 'root',
 	'passwd': 'robertv',
 	'database': 'MITCourseMatch'
 }
-cnx = mysql.connector.connect(**config)
+cnx = mysql.connector.connect(**_config)
 cursor = cnx.cursor()

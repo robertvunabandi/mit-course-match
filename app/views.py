beaf960fd3a70dd1716b9a720b805d2f88f01685
Robert Vunabandi <rvunabandi@gmail.com>
2018-08-05 18:06:47 -0700

    Working on the quiz page where one can answer questions
    Created the quiz page and method to get questions.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,42 @@
-from flask import render_template
+from flask import render_template, request, json
 from app import app
 
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/quiz')
+def quiz():
+	return render_template('quiz.html')
+
+def get_questions():
+	return [
+		{
+			'question': 'hello there',
+			'qid': 35,
+			'answers': [
+				{'choice': 'Lemon', 'aid': 46},
+				{'choice': 'Almond', 'aid': 55}
+			]
+		},
+		{
+			'question': 'hello there',
+			'qid': 35,
+			'answers': [
+				{'choice': 'Lemon', 'aid': 46},
+				{'choice': 'Almond', 'aid': 55}
+			]
+		}
+	]
+
+@app.route('/questions', methods=['GET'])
+def questions():
+	# temporary
+	return json.dumps(get_questions())
+
+@app.errorhandler(404)
+def not_found(error):
+	print(error)
+	return render_template('error.html'), 404

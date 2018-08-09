242d3daab85970480abf4363cb176e84dc7a86a4
Robert Vunabandi <rvunabandi@gmail.com>
2018-08-08 22:06:37 -0700

    Changing python files to use double quotes for consistency with javascript

--- app/views.py
+++ app/views.py
@@ -2,36 +2,36 @@ from flask import render_template, request, json
 from app import app
 
 
-@app.route('/')
+@app.route("/")
 def index():
-	return render_template('index.html')
+	return render_template("index.html")
 
 
-@app.route('/quiz')
+@app.route("/quiz")
 def quiz():
-	return render_template('quiz.html')
+	return render_template("quiz.html")
 
 def get_questions():
 	return [
 		{
-			'question': 'hello there',
-			'qid': 35,
-			'answers': [
-				{'choice': 'Lemon', 'aid': 46},
-				{'choice': 'Almond', 'aid': 55}
+			"question": "hello there",
+			"qid": 35,
+			"answers": [
+				{"choice": "Lemon", "aid": 46},
+				{"choice": "Almond", "aid": 55}
 			]
 		},
 		{
-			'question': 'okay tell me',
-			'qid':24,
-			'answers': [
-				{'choice': 'No', 'aid': 41},
-				{'choice': 'Yes', 'aid': 31}
+			"question": "okay tell me",
+			"qid":24,
+			"answers": [
+				{"choice": "No", "aid": 41},
+				{"choice": "Yes", "aid": 31}
 			]
 		}
 	]
 
-@app.route('/questions', methods=['GET'])
+@app.route("/questions", methods=["GET"])
 def questions():
 	# temporary
 	return json.dumps(get_questions())
@@ -39,4 +39,4 @@ def questions():
 @app.errorhandler(404)
 def not_found(error):
 	print(error)
-	return render_template('error.html'), 404
+	return render_template("error.html"), 404

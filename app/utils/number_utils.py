19a81ba3f88ac92bd8f3377e9e38b5f9b74100b4
Robert Vunabandi <rvunabandi@gmail.com>
2018-08-16 22:40:48 -0700

    changed util into number_utils and created string utils.
    this change cascaded into other files as well

--- app/utils/number_utils.py
+++ app/utils/number_utils.py
@@ -22,25 +22,25 @@ def assert_valid_db_id(i: Any, idtype: str = None) -> None:
 		'%s must be an instance of integer. Got %s' % (idtype, repr(type(i)))
 
 
-def is_integer_in_range_inclusive(i: int, min_: int, max_: int) -> bool:
+def is_integer_in_range_exclusive(i: int, min_: int, max_: int) -> bool:
 	msg = "expecting integer. Instead got %s of type %s"
 	assert is_integer(i), msg % (repr(i), repr(type(i)))
 	assert is_integer(min_), msg % (repr(min_), repr(type(min_)))
 	assert is_integer(max_), msg % (repr(max_), repr(type(max_)))
-	return min_ <= i <= max_
+	return min_ < i < max_
 
 
-def is_integer_in_range_exclusive(i: int, min_: int, max_: int) -> bool:
-	return is_integer_in_range_inclusive(i, min_ - 1, max_ + 1)
+def is_integer_in_range_inclusive(i: int, min_: int, max_: int) -> bool:
+	return is_integer_in_range_exclusive(i, min_ - 1, max_ + 1)
 
 
-def is_in_range_inclusive(f: float, min_: float, max_: float) -> bool:
+def is_in_range_exclusive(f: float, min_: float, max_: float) -> bool:
 	msg = "expecting float. Instead got %s of type %s"
 	assert is_float(f), msg % (repr(f), repr(type(f)))
 	assert is_float(min_), msg % (repr(min_), repr(type(min_)))
 	assert is_float(max_), msg % (repr(max_), repr(type(max_)))
-	return min_ <= f <= max_
+	return min_ < f < max_
 
 
-def is_in_range_exclusive(f: float, min_: float, max_: float) -> bool:
-	return is_in_range_inclusive(f, min_ - FLOAT_DELTA, max_ + FLOAT_DELTA)
+def is_in_range_inclusive(f: float, min_: float, max_: float) -> bool:
+	return is_in_range_exclusive(f, min_ - FLOAT_DELTA, max_ + FLOAT_DELTA)

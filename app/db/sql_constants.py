32c19aef7a3ee4608ca7a215afc0042c8a0a5edf
Robert Vunabandi <rvunabandi@gmail.com>
2018-08-21 22:07:00 -0700

    updated sql constants to match the new format

--- app/db/sql_constants.py
+++ app/db/sql_constants.py
@@ -4,10 +4,6 @@ class TBL:
 	"""
 	Questions = "Questions"
 	AnswerChoices = "AnswerChoices"
-	QuestionSets = "QuestionSets"
-	QuestionMappings = "QuestionMappings"
-	MappingSets = "MappingSets"
-	AnswerMappings = "AnswerMappings"
 	Courses = "Courses"
 	Responses = "Responses"
 	ResponseMappings = "ResponseMappings"
@@ -21,10 +17,6 @@ class TBLCol:
 	question = "question"
 	answer_id = "aid"
 	choice = "choice"
-	question_set_id = "qsid"
-	question_set_name = "qs_name"
-	mapping_set_id = "msid"
-	mapping_set_name = "ms_name"
 	vector = "vector"
 	course_number = "cn"
 	course_name = "course_name"
@@ -46,52 +38,26 @@ class Questions(TableColumns):
 
 class AnswerChoices(TableColumns):
 	def get_columns(self):
-		return TBLCol.answer_id, TBLCol.question_id, TBLCol.choice
-
-
-class QuestionSets(TableColumns):
-	def get_columns(self):
-		return TBLCol.question_set_id, TBLCol.question_set_name
-
-
-class QuestionMappings(TableColumns):
-	def get_columns(self):
-		return TBLCol.question_set_id, TBLCol.question_id
-
-
-class MappingSets(TableColumns):
-	def get_columns(self):
 		return (
-			TBLCol.mapping_set_id,
-			TBLCol.question_set_id,
-			TBLCol.mapping_set_name
-		)
-
-
-class AnswerMappings(TableColumns):
-	def get_columns(self):
-		return (
-			TBLCol.mapping_set_id,
-			TBLCol.question_set_id,
 			TBLCol.answer_id,
-			TBLCol.vector
+			TBLCol.question_id,
+			TBLCol.choice,
+			TBLCol.vector,
 		)
 
 
 class Courses(TableColumns):
 	def get_columns(self):
-		return TBLCol.course_number, TBLCol.course_name, TBLCol.course_id
+		return TBLCol.course_id, TBLCol.course_number, TBLCol.course_name
 
 
 class Responses(TableColumns):
 	def get_columns(self):
 		return (
 			TBLCol.response_id,
-			TBLCol.question_set_id,
 			TBLCol.course_number,
 			TBLCol.time_created,
-			TBLCol.course_id,
-			TBLCol.response_salt
+			TBLCol.response_salt,
 		)
 
 
